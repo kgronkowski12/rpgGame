0a4b5817471a9cf6c865a1817894815e4b681af9
bszw123 <85299264+bszw123@users.noreply.github.com>
2023-11-06 21:03:01 +0100

    Update tile.py

--- tile.py
+++ tile.py
@@ -2,9 +2,12 @@ import pygame
 from settings import *
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self,position,group):
+    def __init__(self,position,group,sprite_type,surface=pygame.Surface((TILESIZE,TILESIZE))):
         super().__init__(group)
-        self.image = pygame.image.load('../img/test/rock.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft=position) #cały rozmiar
-        self.hitbox = self.rect.inflate(0,-10) #hitbox będzie mniejszy niż postać (taki jakby mniejszy prostokąt w centrum, pierwsza wspolrzedna sie nie zmienia
-        #wiec hitbox dotyka boków sprite'a aleee nie góry/dołu)
+        self.sprite_type = sprite_type
+        self.image = surface
+        if sprite_type == 'object':
+            self.rect = self.image.get_rect(topleft= (position[0],position[1] - TILESIZE)) #odejmujemy tile bo duze obiekty maja 128x64 lub 64x128 wiec 'topleft' jest przesuwany na srodek
+        else:
+            self.rect = self.image.get_rect(topleft=position) #cały rozmiar
+        self.hitbox = self.rect.inflate(0,-10) #stworzenie hitboxu
