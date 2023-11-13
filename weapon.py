d32daaf089433b78e0c3a078f6604bd8521ecb7b
bszw123 <85299264+bszw123@users.noreply.github.com>
2023-11-13 17:23:46 +0100

    Add files via upload

---
+++ weapon.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Weapon(pygame.sprite.Sprite):
+    def __init__(self,player,groups):
+        super().__init__(groups)
+        direction = player.status.split('_')[0] #chcemy się pozbyć stanów idle z listy stanów tak by dostać tylko kierunek
+
+
+        #grafika
+        full_path = f'../img/weapons/{player.weapon}/{direction}.png'
+        self.image = pygame.image.load(full_path).convert_alpha()
+        
+        
+        #położenie broni
+        if direction == 'right':
+            self.rect = self.image.get_rect(midleft = player.rect.midright + pygame.math.Vector2(0,20)) #dodajemy offset by broń była bliżej dloni
+        elif direction == 'left':
+            self.rect = self.image.get_rect(midright = player.rect.midleft + pygame.math.Vector2(4,20))
+        
+        elif direction == 'down':
+            self.rect = self.image.get_rect(midtop = player.rect.midbottom + pygame.math.Vector2(-10,0))
+        else:
+            self.rect = self.image.get_rect(midbottom = player.rect.midtop + pygame.math.Vector2(16,0))
