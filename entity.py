9670d246f393e2d83f441d2599b98346ba29525e
bszw123 <85299264+bszw123@users.noreply.github.com>
2023-11-13 17:27:05 +0100

    Add files via upload

---
+++ entity.py
@@ -0,0 +1,34 @@
+import pygame
+
+class Entity(pygame.sprite.Sprite):
+    def __init__(self,groups):
+        super().__init__(groups)
+        self.frame_index = 0
+        self.animations_speed = 0.15
+        self.direction = pygame.math.Vector2()
+
+    def move(self,speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize() #aby prędkość nie była podwójna przy trzymaniu dwóch kierunków
+            #jednocześnie np. dół i prawo
+        self.hitbox.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.hitbox.y += self.direction.y * speed
+        self.collision('vertical')
+        self.rect.center = self.hitbox.center
+
+    def collision(self,direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.x > 0: #idziemy w prawo
+                        self.hitbox.right = sprite.rect.left #skoro idziemy w prawo to ewentualna kolizja na 100% będzie po prawej stronie
+                    if self.direction.x < 0:
+                        self.hitbox.left = sprite.rect.right
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if self.direction.y > 0: #idziemy w dół, w pygame center point (0,0) jest w gornym lewym rogu
+                        self.hitbox.bottom = sprite.rect.top
+                    if self.direction.y < 0:
+                        self.hitbox.top = sprite.rect.bottom
