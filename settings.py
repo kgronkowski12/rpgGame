5ca8212c8a772ca950b0816a678c4ac71b82e9fa
bszw123 <85299264+bszw123@users.noreply.github.com>
2023-11-22 17:29:40 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,10 +4,10 @@ FPS = 60
 TILESIZE = 64
 
 #ui
-BAR_HEIGHT = 20
+BAR_HEIGHT = 16
 HEALTH_BAR_WIDTH = 200
-ENERGY_BAR_WIDTH = 140
-ITEM_BOX_SIZE = 80
+ENERGY_BAR_WIDTH = 200
+ITEM_BOX_SIZE = 64
 UI_FONT = '../img/font/Minecraft.ttf'
 UI_FONT_SIZE = 18
 
@@ -15,13 +15,11 @@ UI_FONT_SIZE = 18
 WATER_COLOR = '#71ddee'
 UI_BG_COLOR = '#222222'
 UI_BORDER_COLOR = '#111111'
-TEXT_COLOR = '#EEEEEE'
+TEXT_COLOR = 'white'
 
 #kolory ui
 HEALTH_COLOR = '#800a01'
 ENERGY_COLOR = '#010a70'
-UI_BORDER_COLOR_ACTIVE = 'gold'
-
 
 #bronie
 weapon_data = {
@@ -39,6 +37,6 @@ magic_data = {
 
 #wrogowie;  resistence to odleglosc na jaka są odrzuceni gdy gracz ich zaatakuje
 monster_data = {
-	'bear_dog': {'health': 100,'exp':100,'damage':20,'attack_type': 'bite', 'attack_sound':'../sound/attack/bite.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
-	'mushroom': {'health': 100,'exp':110,'damage':8,'attack_type': 'cut', 'attack_sound':'../sound/attack/mushroom.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
+	'bear_dog': {'health': 100,'exp':100,'damage':20,'attack_type': 'bite', 'attack_sound':'../sound/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 80, 'notice_radius': 360},
+	'mushroom': {'health': 100,'exp':110,'damage':8,'attack_type': 'cut', 'attack_sound':'../sound/attack/fireball.wav', 'speed': 4, 'resistance': 3, 'attack_radius': 60, 'notice_radius': 350},
 	'skeletor': {'health': 70,'exp':120,'damage':6,'attack_type': 'bone', 'attack_sound':'../sound/attack/slash.wav', 'speed': 3, 'resistance': 3, 'attack_radius': 50, 'notice_radius': 300}}
