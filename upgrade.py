2d010bc7c439e9e0c3823370901e85b84b4a6c6d
bszw123 <85299264+bszw123@users.noreply.github.com>
2023-12-18 18:30:52 +0100

    Update upgrade.py

--- upgrade.py
+++ upgrade.py
@@ -11,7 +11,7 @@ class Upgrade:
         self.attribute_nr = len(player.stats) #ilość statystyk gracza
         self.attribute_names = list(player.stats.keys())
         self.max_values = list(player.max_stats.values())
-        self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
+        self.font = pygame.font.Font(FONT_UI,FONT_UI_SIZE)
 
         #rozmiar itp
         self.height = self.display_surface.get_size()[1] *0.8 #cały ekran i 20%
@@ -24,7 +24,7 @@ class Upgrade:
         self.selection_time = None
         self.can_move = True
 
-    def input(self):
+    def player_input(self):
         keys = pygame.key.get_pressed()
 
         if self.can_move:
@@ -32,15 +32,15 @@ class Upgrade:
                 self.selection_index += 1
                 self.can_move = False
                 self.selection_time = pygame.time.get_ticks()
-            elif keys[pygame.K_LEFT] and self.selection_index > 1:
+            elif keys[pygame.K_LEFT] and self.selection_index > 0:
                 self.selection_index -= 1
                 self.can_move = False
                 self.selection_time = pygame.time.get_ticks()
         
-            if keys[pygame.K_SPACE]:
+            if keys[pygame.K_UP]:
                 self.can_move = False
                 self.selection_time = pygame.time.get_ticks()
-                print(self.selection_index)
+                self.item_list[self.selection_index].trigger(self.player)
 
     def selection_cooldown(self):
         if not self.can_move:
@@ -53,13 +53,9 @@ class Upgrade:
         self.item_list = []
 
         for item, index in enumerate(range(self.attribute_nr)):
-            #horizontal
             full_width = self.display_surface.get_size()[0]
             increment = full_width // self.attribute_nr
-            left = (item * increment) + (increment-self.width) //2 #kazdy element obok bedzie mial delikatny offset
-
-
-            #vertical
+            left = (item * increment) + (increment-self.width) //2 #kazdy element obok bedzie mial delikatny shift
             top = self.display_surface.get_size()[1]*0.1
             #ustalilismy ze chcemy 80% ekranu, czyli 10% na dole i 10 na gorze to bedzie wolne miejsce
             #jako że potrzeba nam po prostu punktu na gorze od ktorego rysujemy
@@ -69,7 +65,7 @@ class Upgrade:
 
 
     def display(self):
-        self.input()
+        self.player_input()
         self.selection_cooldown()
 
         for index, item in enumerate(self.item_list):
@@ -87,23 +83,50 @@ class Item:
         self.font = font
 
     def display_names(self,surface,name,cost,selected):
-        color = TEXT_COLOR_SELECTED if selected else TEXT_COLOR
+        colour = COLOUR_TEXT_SELECTED if selected else COLOUR_TEXT_UPGRADE
 
         #tytuły
-        title_surf = self.font.render(name,False,color)
+        title_surf = self.font.render(name,False,colour)
         title_rect = title_surf.get_rect(midtop = self.rect.midtop + pygame.math.Vector2(0,20))
 
         #koszt
-        cost_surf = self.font.render(f'{int(cost)}',False,color) #int bo jest szansa ze koszt bedzie floatem a potem zmieniamy w f stringa
+        cost_surf = self.font.render(f'{int(cost)}',False,colour) #int bo jest szansa ze koszt bedzie floatem a potem zmieniamy w f stringa
         cost_rect = cost_surf.get_rect(midbottom = self.rect.midbottom - pygame.math.Vector2(0,20))
 
         #rysujemy
         surface.blit(title_surf,title_rect)
         surface.blit(cost_surf,cost_rect)
 
+    def display_bar(self,surface,value,max_value,selected):
+        top = self.rect.midtop + pygame.math.Vector2(0,60)
+        bottom = self.rect.midbottom - pygame.math.Vector2(0,60)
+        colour = COLOUR_BAR_SELECTED if selected else COLOUR_BAR
+
+        full_height = bottom[1] - top[1]
+        relative_number = (value/max_value) * full_height
+        value_rect = pygame.Rect(top[0] - 15,bottom[1]-relative_number,30,10) #left,top,width,height,15 bo polowa 30
+        #relative to ile od paska aktualnego ulepszenia jest w doł do spodu
+        pygame.draw.line(surface,colour,top,bottom,5)
+        pygame.draw.rect(surface,colour,value_rect)
+
+
+    def trigger(self,player):
+        upgrade_attribute = list(player.stats.keys())[self.index]
+
+        if player.xp >= player.upgrade_cost[upgrade_attribute] and player.stats[upgrade_attribute] < player.max_stats[upgrade_attribute]:
+            player.xp -= player.upgrade_cost[upgrade_attribute]
+            player.stats[upgrade_attribute] *= 1.2
+            player.upgrade_cost[upgrade_attribute] *= 1.4
+
+        if player.stats[upgrade_attribute] > player.max_stats[upgrade_attribute]:
+            player.stats[upgrade_attribute] = player.max_stats[upgrade_attribute]
 
     def display(self,surface,selection_num,name,value,max_value,cost):
         if self.index == selection_num:
-            pass
-        pygame.draw.rect(surface,UI_BG_COLOR,self.rect)
+            pygame.draw.rect(surface,COLOUR_UPGRADE_BG_SELECTED,self.rect)
+            pygame.draw.rect(surface,'white',self.rect,4)
+        else:
+            pygame.draw.rect(surface,COLOUR_UPGRADE_BG,self.rect)
+            pygame.draw.rect(surface,'black',self.rect,4)
         self.display_names(surface,name,cost,self.index == selection_num)
+        self.display_bar(surface,value,max_value,self.index == selection_num)
