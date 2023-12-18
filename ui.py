a19bafe13f1f181b5b87e407ac5a82e0b10fe319
bszw123 <85299264+bszw123@users.noreply.github.com>
2023-12-18 18:27:35 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -4,52 +4,51 @@ from settings import *
 class UI:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
-        self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
+        self.font = pygame.font.Font(FONT_UI,FONT_UI_SIZE)
 
         #paski i statystyki
-        self.health_bar_rect = pygame.Rect(8,668,HEALTH_BAR_WIDTH,BAR_HEIGHT)
-        self.energy_bar_rect = pygame.Rect(8,685,ENERGY_BAR_WIDTH,BAR_HEIGHT)
+        self.health_bar_rect = pygame.Rect(8,668,BAR_WIDTH,BAR_HEIGHT)
+        self.mana_bar_rect = pygame.Rect(8,685,BAR_WIDTH,BAR_HEIGHT)
 
         #konwertowanie słownika z broniami
         self.weapon_graphics = []
-        for weapon in weapon_data.values():
+        for weapon in info_weapons.values():
             path = weapon['graphic']
             weapon = pygame.image.load(path).convert_alpha()
             self.weapon_graphics.append(weapon)
 
         self.magic_graphics = []
-        for magic in magic_data.values():
+        for magic in info_magic.values():
             magic = pygame.image.load(magic['graphic']).convert_alpha()
             self.magic_graphics.append(magic)
 
-    def show_bar(self,current,max_amount,bg_rect,color):
+    def show_bar(self,current,max_amount,bg_rect,colour):
         #tło dla paska
-        pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
-
+        pygame.draw.rect(self.display_surface,COLOUR_UI_BG,bg_rect)
         #konwertowanie statystyk do pikseli
         ratio = current / max_amount #np obecne zycie/max zycie 100/100 = 1
         current_width = bg_rect.width * ratio #pasek 200px*1 ratio =200px (wiec caly pasek jest wypelniony zyciem)
         current_rect = bg_rect.copy() #juz wiekszosc info jest w bg_rect wiec to kopiuje po prostu
         current_rect.width = current_width
         #rysujemy pasek
-        pygame.draw.rect(self.display_surface,color,current_rect)
-        pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,bg_rect,4) #ostatni argument (4) sprawia że pozbywamy się wypełnienia i zostaje
+        pygame.draw.rect(self.display_surface,colour,current_rect)
+        pygame.draw.rect(self.display_surface,COLOUR_UI_BORDER,bg_rect,4) #ostatni argument (4) sprawia że pozbywamy się wypełnienia i zostaje
         #tylko krawędź na tyle duża jaką liczbę podaliśmy
 
-    def show_exp(self,exp):
-        text_surf = self.font.render(str(int(exp)),False,TEXT_COLOR) #false bo zadnego anty aliasingu nie chcemy bo mamy pixelart DOCH
-        #exp zmieniamy w str bo to ma byc info ale wczesniej na inta zeby sie pozbyc ewentualnych dziwnych koncowek np 4.12341242 w expie
-        x = self.display_surface.get_size()[0] - 20
-        y = self.display_surface.get_size()[1] - 20
-        text_rect = text_surf.get_rect(bottomright = (x,y))
+    def show_xp(self,xp):
+        text_surf = self.font.render(str(int(xp)),False,COLOUR_TEXT) #false bo zadnego anty aliasingu nie chcemy bo mamy pixelart DOCH
+        #xp zmieniamy w str bo to ma byc info ale wczesniej na inta zeby sie pozbyc ewentualnych dziwnych koncowek np 4.12341242 w xpie
+        x = self.display_surface.get_size()[0] - 1262
+        y = self.display_surface.get_size()[1] - 152
+        text_rect = text_surf.get_rect(topleft = (x,y))
         
-        pygame.draw.rect(self.display_surface,UI_BG_COLOR,text_rect.inflate(20,20))
+        pygame.draw.rect(self.display_surface,COLOUR_UI_BG,text_rect.inflate(20,20))
         self.display_surface.blit(text_surf,text_rect)
-        pygame.draw.rect(self.display_surface,UI_BORDER_COLOR,text_rect.inflate(20,20),3)
+        pygame.draw.rect(self.display_surface,COLOUR_UI_BORDER,text_rect.inflate(20,20),3)
 
     def selection_box(self,left,top,has_switched):
-        bg_rect = pygame.Rect(left,top,ITEM_BOX_SIZE,ITEM_BOX_SIZE)
-        pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
+        bg_rect = pygame.Rect(left,top,BOX,BOX)
+        pygame.draw.rect(self.display_surface,COLOUR_UI_BG,bg_rect)
         return bg_rect
 
     def weapon_overlay(self,weapon_index,has_switched):
@@ -64,14 +63,13 @@ class UI:
         bg_rect = self.selection_box(73,600,has_switched) # magia
         magic_surf = self.magic_graphics[magic_index]
         magic_rect = magic_surf.get_rect(center = bg_rect.center)
-
         self.display_surface.blit(magic_surf,magic_rect)
 
 
     def display(self,player):
-        self.show_bar(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
-        self.show_bar(player.energy,player.stats['energy'],self.energy_bar_rect,ENERGY_COLOR)
+        self.show_bar(player.hp,player.stats['hp'],self.health_bar_rect,COLOUR_HEALTH)
+        self.show_bar(player.mana,player.stats['mana'],self.mana_bar_rect,COLOUR_MANA)
 
-        self.show_exp(player.exp)
+        self.show_xp(player.xp)
         self.weapon_overlay(player.weapon_index,not player.can_switch_weapon)
         self.magic_overlay(player.magic_index, not player.can_switch_magic)
