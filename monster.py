aa5f9357539f6a5b09d23d88adae95e371a17611
bszw123 <85299264+bszw123@users.noreply.github.com>
2024-01-15 13:02:22 +0100

    Update and rename enemy.py to monster.py

--- monster.py
+++ monster.py
@@ -3,20 +3,24 @@ from settings import *
 from math import sin
 
 
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self,monster_name,position,group,obstacle_sprites,damage_player,trigger_death_elements,add_xp):
-        super().__init__(group)
-        self.label_sprite = 'enemy'
+class Monster(pygame.sprite.Sprite):
+    def __init__(self,monster_name,position,label,obstacle_sprites,damage_froggo,trigger_death_elements,add_xp):
+        super().__init__(label)
+        
+        self.category = 'monster' #typ jest po to
+        #aby mialy rozne wartosci i reakcje, np gracz atakuje wroga (traci zdrowie a potem umiera)
+        #gracz atakuje kwiatka (nie ma zycia ale od razu zostaje zniszczone) itp
+
         #grafika
         self.import_graphics(monster_name)
         self.status = 'idle'
         self.frame_index = 0
         self.animation_speed = 0.15
-        self.image = self.animation_status[self.status][self.frame_index]
+        self.graphic = self.animation_status[self.status][self.frame_index]
 
 
         #poruszanie sie
-        self.rect = self.image.get_rect(topleft = position)
+        self.rect = self.graphic.get_rect(topleft = position)
         self.hitbox = self.rect.inflate(0,-10)
         self.obstacle_sprites = obstacle_sprites
         self.direction = pygame.math.Vector2()
@@ -24,7 +28,7 @@ class Enemy(pygame.sprite.Sprite):
 
         #statystyki
         self.monster_name = monster_name
-        monster_info = info_monster[self.monster_name]
+        monster_info = INFO_MONSTER[self.monster_name]
         self.hp = monster_info['hp']
         self.xp= monster_info['xp']
         self.speed= monster_info['speed']
@@ -38,7 +42,7 @@ class Enemy(pygame.sprite.Sprite):
         self.can_attack = True
         self.attack_time = None
         self.attack_cooldown = 400
-        self.damage_player = damage_player
+        self.damage_froggo = damage_froggo
         self.trigger_death_elements = trigger_death_elements
         self.add_xp = add_xp
 
@@ -53,16 +57,16 @@ class Enemy(pygame.sprite.Sprite):
         self.death_sound = pygame.mixer.Sound('../sound/death.wav')
         self.hit_sound = pygame.mixer.Sound('../sound/hit.wav')
         self.attack_sound = pygame.mixer.Sound(monster_info['attack_sound'])
-        self.death_sound.set_volume(0.6)
-        self.hit_sound.set_volume(0.6)
-        self.attack_sound.set_volume(0.3)
+        self.death_sound.set_volume(0.5)
+        self.hit_sound.set_volume(0.5)
+        self.attack_sound.set_volume(0.2)
 
-    def import_graphics(self,name):
+    def import_graphics(self,name): #podobnie jak import froggo assets z self animations
         self.animation_status = {'idle':[],'move':[],'attack':[]}
-        main_path = f'../img/enemies/{name}/'
+        main_folder = f'../img/enemies/{name}/'
         for animation in self.animation_status.keys():
-            self.animation_status[animation] = folder_import(main_path + animation)
-            #main path to ta sciezka wyzej, animation to idle/move/attack
+            self.animation_status[animation] = folder_import(main_folder + animation)
+            #main folder to ta sciezka wyzej, animation to idle/move/attack
             #laczymy je i dostajemy sciezke do konkretnego folderu
             #i potem funkcja import folder daje nam kazde zdjecie w danym folderze
             #i mozna walnac je do naszego słownika
@@ -99,24 +103,26 @@ class Enemy(pygame.sprite.Sprite):
         value = sin(pygame.time.get_ticks())
         if value >= 0: return 255 #pelna przezroczystosc
         else: return 0
-
-
-
-    def get_player_distance_direction(self,player): #może to rozbić te funkcje
-        enemy_vec = pygame.math.Vector2(self.rect.center)
-        player_vec = pygame.math.Vector2(player.rect.center)
-        distance = (player_vec - enemy_vec).magnitude() #(odejmujemy wektory gracza i wroga od poczatku ukladu wspolrzednych)
+    
+    def froggo_distance(self,froggo):
+        vector = (pygame.math.Vector2(froggo.rect.center) - pygame.math.Vector2(self.rect.center)) #(odejmujemy wektory gracza i wroga od poczatku ukladu wspolrzednych)
         # a dostajemy wektor miedzy wrogiem a graczem i potem za pomoca magnitude zmieniamy wektor w odleglosc
+        distance = vector.magnitude()
+        return (distance)
+    
+    def froggo_direction(self,froggo):
+        distance = self.froggo_distance(froggo)
         if distance > 0:
-            direction = (player_vec - enemy_vec).normalize()
+            direction = (pygame.math.Vector2(froggo.rect.center) - pygame.math.Vector2(self.rect.center)).normalize()
         else:
             direction = pygame.math.Vector2() # jeśli gracz i wrog sa w tym samym miejscu to dajemy wrogowi po prostu wektor (0,0)
-        return (distance,direction)
+        return (direction)
 
 
 
-    def get_status(self,player):
-        distance = self.get_player_distance_direction(player)[0]
+
+    def get_status(self,froggo):
+        distance = self.froggo_distance(froggo)
 
         if distance <= self.range_attack and self.can_attack:
             if self.status != 'attack':
@@ -128,33 +134,33 @@ class Enemy(pygame.sprite.Sprite):
             self.status = 'idle'
     
 
-    def actions(self,player):
+    def actions(self,froggo):
         if self.status == 'attack':
             self.attack_time = pygame.time.get_ticks()
-            self.damage_player(self.attack_damage,self.attack_type)
+            self.damage_froggo(self.attack_damage,self.attack_type)
             self.attack_sound.play()
         elif self.status == 'move':
-            self.direction = self.get_player_distance_direction(player)[1]
+            self.direction = self.froggo_direction(froggo)
         else:
             self.direction = pygame.math.Vector2() #gdy gracz wyjdzie z zasiegu wzroku wroga, zatrzyma się on odrazu w miejscu
 
-    def create_animation(self): # prawie to samo co funcja create_animation w graczu
+    def create_animation(self):
         animation = self.animation_status[self.status]
 
-        self.frame_index += self.animation_speed #bierzemy to z entity
+        self.frame_index += self.animation_speed
         if self.frame_index >= len(animation):
             if self.status == 'attack':
                 self.can_attack = False
             self.frame_index = 0
 
-        self.image = animation[int(self.frame_index)]
-        self.rect = self.image.get_rect(center = self.hitbox.center) #ruszamy hitbox a nie rectangle,wiec 'wkladamy' rectangle w srodek hitboxa
+        self.graphic = animation[int(self.frame_index)]
+        self.rect = self.graphic.get_rect(center = self.hitbox.center) #ruszamy hitbox a nie rectangle,wiec 'wkladamy' rectangle w srodek hitboxa
 
         if not self.vulnerable: #gdy wrog jest atakowany
             alpha = self.wave_value()
-            self.image.set_alpha(alpha)
+            self.graphic.set_alpha(alpha)
         else:
-            self.image.set_alpha(255) #set alpha to przezroczystość (255 to pelna), czyli gdy nie jest atakowany to widac caly czas w 100%
+            self.graphic.set_alpha(255) #set alpha to przezroczystość (255 to pelna), czyli gdy nie jest atakowany to widac caly czas w 100%
 
     def cooldowns(self):
         current_time = pygame.time.get_ticks()
@@ -169,14 +175,14 @@ class Enemy(pygame.sprite.Sprite):
 
 
 
-    def get_damage(self,player,attack_type):
+    def get_damage(self,froggo,attack_type):
         if self.vulnerable:
             self.hit_sound.play()
-            self.direction = self.get_player_distance_direction(player)[1] #przesuwamy wroga w inny kierunek
+            self.direction = self.froggo_direction(froggo) #przesuwamy wroga w inny kierunek
             if attack_type == 'weapon':
-                self.hp -= player.get_full_weapon_damage() #getter funkcja
+                self.hp -= froggo.damage()[0] #getter funkcja
             else:
-                self.hp -= player.get_full_magic_damage()
+                self.hp -= froggo.damage()[1]
             self.hit_time = pygame.time.get_ticks()
             self.vulnerable = False
 
@@ -200,6 +206,6 @@ class Enemy(pygame.sprite.Sprite):
         self.create_animation()
         self.cooldowns()
         self.check_death()
-    def enemy_update(self,player):
-        self.get_status(player)
-        self.actions(player)
+    def monster_update(self,froggo):
+        self.get_status(froggo)
+        self.actions(froggo)
